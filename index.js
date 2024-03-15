9339a6c072f05ba4948b9001473e35bdfb431847
Iulik <inicula1989@gmail.com>
2024-03-15 22:23:24 +0200

    first

---
+++ index.js
@@ -0,0 +1,71 @@
+// const closure = () => {
+//     const initialValue = 12;
+//     let x = initialValue
+
+//     function showX() {
+//         return x;
+//     }
+
+//     function addThree(){
+//         x = x + 3
+//     }
+
+//     function multiplyByThree() {
+//         x = x * 3
+//     }
+
+//     function reset() {
+//         x = initialValue;
+//     }
+
+//     return {showX, addThree, multiplyByThree, reset}    
+// }
+
+// const { showX, addThree, multiplyByThree, reset } = closure()
+
+// addThree()
+// addThree()
+// addThree()
+
+// console.log(showX(), '1')
+
+// multiplyByThree()
+
+// console.log(showX(), '2')
+
+// reset()
+
+// console.log(showX(), '3')
+
+
+const arr = [1,2,3,4,5,6]
+// const arr1 = [7,8,9]
+
+// const returnValue = arr.push(7)
+
+// console.log(returnValue)
+
+// const concatResult = arr.concat(arr1)
+
+// console.log(concatResult)
+// console.log(arr)
+// console.log(arr1)
+
+// const mapedArr = arr.map((item, index) => {
+//     return <h2>{item}</h2>;
+// })
+
+// console.log(mapedArr)
+
+
+
+// const arr = [1,2,3,4,5,6]
+
+
+
+
+// arr.length = 3
+
+// console.log(arr)
+
+
